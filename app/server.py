53aad5a127bf44421a7a0a2b076e822315842c59
Ubuntu <labuser@ubuntugui.vlqet1yfde1u3mqjj3nxgcmtsf.tx.internal.cloudapp.net>
2023-07-14 07:30:48 +0000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ library = Library()
 @app.route("/library/book/<int:isbn>", methods=['GET'])
 def find(isbn):
     try:
-	print('fetching')
+        print('fetching')
         book = library.find(isbn)
         response = make_response(jsonify({
             "isbn": book.isbn,
